3bcfc5d0d49468f3d76d22a0a8de562f77f6987d
2331771385 <2331771385@qq.comm>
2021-12-26 12:34:24 +0800

    生成留言&全景&访问量统计

--- vue.config.js
+++ vue.config.js
@@ -35,7 +35,7 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://192.168.0.109:8080`,
+        target: `http://192.168.0.109:8081`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
